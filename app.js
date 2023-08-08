0cc982e4c8130caab9d2e5d7adb5088517dae3d8
lingfeng20222 <120234342+lingfeng20222@users.noreply.github.com>
2023-08-08 18:44:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { TextDecoder } = require('util');
 const logcb= (...args)=>console.log.bind(this,...args);
 const errcb= (...args)=>console.error.bind(this,...args);
 
-const uuid= (process.env.UUID||'d342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, "");
+const uuid= (process.env.UUID||'af7eb4cd-da21-4f5f-90e9-dbb400a966b0').replace(/-/g, "");
 const port= process.env.PORT||3000;
 
 const wss=new WebSocket.Server({port},logcb('listen:', port));
